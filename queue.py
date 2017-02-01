8ee90541de2861b171e4e6691e3a1c0fc5c104b3
Sam Lee <samlee405@yahoo.com>
2017-02-01 13:01:05 -0800

    queue with arrays and linked list implemented

--- queue.py
+++ queue.py
@@ -1,42 +1,103 @@
 #!python
+from linkedlist import LinkedList
 
-class Queue(object):
+# class Queue(object):
+#
+#     def __init__(self, iterable=None):
+#         """Initialize this queue and enqueue the given items, if any"""
+#         self.queue = []
+#         self.queueLength = 0
+#         if iterable:
+#             for item in iterable:
+#                 self.enqueue(item)
+#
+#     def __repr__(self):
+#         """Return a string representation of this queue"""
+#         return 'Queue({})'.format(self.length())
+#
+#     def is_empty(self):
+#         """Return True if this queue is empty, or False otherwise"""
+#         if len(self.queue) == 0:
+#             return True
+#         else:
+#             return False
+#
+#     def length(self):
+#         """Return the number of items in this queue"""
+#         return self.queueLength
+#
+#     def peek(self):
+#         """Return the next item in this queue without removing it,
+#         or None if this queue is empty"""
+#         if self.queueLength != 0:
+#             return self.queue[0]
+#         else:
+#             return None
+#
+#     def enqueue(self, item):
+#         """Enqueue the given item into this queue"""
+#         self.queue.append(item)
+#         self.queueLength += 1
+#
+#     def dequeue(self):
+#         """Return the next item and remove it from this queue,
+#         or raise ValueError if this queue is empty"""
+#
+#         if self.queueLength == 0:
+#             raise ValueError
+#         else:
+#             valueToReturn = self.queue[0]
+#             del self.queue[0]
+#             self.queueLength -= 1
+#
+#             return valueToReturn
+
+
+class Queue(LinkedList):
 
     def __init__(self, iterable=None):
         """Initialize this queue and enqueue the given items, if any"""
-        # TODO: initialize instance variables
-        pass
+        super(Queue, self).__init__()
         if iterable:
             for item in iterable:
                 self.enqueue(item)
 
     def __repr__(self):
         """Return a string representation of this queue"""
-        return 'Queue({})'.format(self.length())
+        # return 'Queue({})'.format(self)
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise"""
-        # TODO: check if empty
-        pass
+        if self.length == 0:
+            return True
+        else:
+            return False
 
-    def length(self):
-        """Return the number of items in this queue"""
-        # TODO: count number of items
-        pass
+    # def length(self):
+    #     """Return the number of items in this queue"""
+    #     return self.length
 
     def peek(self):
         """Return the next item in this queue without removing it,
         or None if this queue is empty"""
-        # TODO: return next item, if any
-        pass
+        if self.length != 0:
+            return self.head.data
+        else:
+            return None
 
     def enqueue(self, item):
         """Enqueue the given item into this queue"""
-        # TODO: enqueue given item
-        pass
+        self.append(item)
 
     def dequeue(self):
         """Return the next item and remove it from this queue,
         or raise ValueError if this queue is empty"""
-        # TODO: dequeue next item, if any
-        pass
+
+        if self.length == 0:
+            raise ValueError
+        else:
+            valueToReturn = self.head.data
+            self.head = self.head.next
+            self.length -= 1
+
+            return valueToReturn
--- test_queue.py
+++ test_queue.py
@@ -9,26 +9,26 @@ class QueueTest(unittest.TestCase):
     def test_init(self):
         q = Queue()
         assert q.peek() is None
-        assert q.length() == 0
+        assert q.length == 0
         assert q.is_empty() is True
 
     def test_init_with_list(self):
         q = Queue(['A', 'B', 'C'])
         assert q.peek() == 'A'
-        assert q.length() == 3
+        assert q.length == 3
         assert q.is_empty() is False
 
     def test_length(self):
         q = Queue()
-        assert q.length() == 0
+        assert q.length == 0
         q.enqueue('A')
-        assert q.length() == 1
+        assert q.length == 1
         q.enqueue('B')
-        assert q.length() == 2
+        assert q.length == 2
         q.dequeue()
-        assert q.length() == 1
+        assert q.length == 1
         q.dequeue()
-        assert q.length() == 0
+        assert q.length == 0
 
     def test_peek(self):
         q = Queue()
@@ -46,23 +46,23 @@ class QueueTest(unittest.TestCase):
         q = Queue()
         q.enqueue('A')
         assert q.peek() == 'A'
-        assert q.length() == 1
+        assert q.length == 1
         q.enqueue('B')
         assert q.peek() == 'A'
-        assert q.length() == 2
+        assert q.length == 2
         q.enqueue('C')
         assert q.peek() == 'A'
-        assert q.length() == 3
+        assert q.length == 3
         assert q.is_empty() is False
 
     def test_dequeue(self):
         q = Queue(['A', 'B', 'C'])
         assert q.dequeue() == 'A'
-        assert q.length() == 2
+        assert q.length == 2
         assert q.dequeue() == 'B'
-        assert q.length() == 1
+        assert q.length == 1
         assert q.dequeue() == 'C'
-        assert q.length() == 0
+        assert q.length == 0
         assert q.is_empty() is True
         with self.assertRaises(ValueError):
             q.dequeue()
